40b21b3d88457bdbfe61abea0281fd5b16e8edec
Nienke <32365843+Artemiss0@users.noreply.github.com>
2018-12-10 16:39:48 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+let express = require('express'),
+    mongoose = require('mongoose'),
+    bodyParser = require('body-parser');
+
+let db = mongoose.connect('mongodb://localhost/bookAPI');
+
+let Book = require('./Models/bookModel');
+
+let app = express();
+
+let port = process.env.PORT || 2000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+bookRouter = require('./Routes/bookRoutes')(Book);
+
+app.use('/api/books', bookRouter);
+// app.use('/api/author', authorRouter);
+
+app.get('/', function (req, res) {
+   res.send('Welcome to my API!!');
+});
+app.listen(port, function () {
+   console.log('Running on PORT: ' + port);
+});
