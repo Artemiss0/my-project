c53b500f237cd784a4d7c3ba275dbab0f4601739
Nienke - <nm.overmeer@gmail.com>
2018-12-17 14:11:49 +0100

    test

--- app.js
+++ app.js
@@ -11,8 +11,6 @@ if(process.env.ENV === 'Test') {
 }else {
    db = mongoose.connect('mongodb://localhost/taskAPI');
 }
-
-
 let app = express();
 
 let port = process.env.PORT || 8000;
@@ -20,12 +18,19 @@ let port = process.env.PORT || 8000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
+app.options(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With", "Content-Type, Authorization, Content-Length, X-Requested-With");
+    res.send(200);
+});
+
 taskRouter = require('./Routes/taskRoutes')(Task);
 
 app.use('/api/tasks', taskRouter);
 
-app.get('/', function (req, res) {
-   res.send('Welcome to my API!! Check');
+app.get('/', function (req, res, next) {
+   res.send('Welcome to my API!! Checker');
 });
 app.listen(port, function () {
    console.log('Running on PORT: ' + port);
